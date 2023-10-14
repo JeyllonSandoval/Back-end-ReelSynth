4c3c56511d524228443b8d3e8b50f9401ab3df55
jcorona48 <105407871+jcorona48@users.noreply.github.com>
2023-10-14 02:03:42 -0400

    Agregando variable Admin_name

--- utils/auth.js
+++ utils/auth.js
@@ -2,7 +2,7 @@
 export const verifyAdmin = (user) => {
     if(!user) throw new Error("No se ha enviado un usuario")
 
-    if(user.role.name !== "Admin") throw new Error("No tienes permisos para realizar esta accion")
+    if(user.role.name !== (process.env.ADMIN_NAME || "Admin") ) throw new Error("No tienes permisos para realizar esta accion")
 
     return true
 }
