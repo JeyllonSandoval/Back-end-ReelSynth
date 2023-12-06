2ec124e6a8bee4a8fc65434cf42546a30a033459
Joan Corona <joaneliasc86@gmail.com>
2023-12-06 15:12:10 -0400

    fix stuido unique

--- Models/Studio.js
+++ Models/Studio.js
@@ -3,7 +3,6 @@ import {Schema, model} from 'mongoose'
 const studioSchema = new Schema({
     name: {
         type: String,
-        unique: true,
         required: true,
         trim: true
     },
