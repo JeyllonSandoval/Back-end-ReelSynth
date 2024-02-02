998fb4a91cc9f8c6305250e85c86cd82bcae05d1
Joan Corona <joaneliasc86@gmail.com>
2024-02-02 13:21:34 -0400

    fix bug for vercel upload

--- db.js
+++ db.js
@@ -4,10 +4,7 @@ import {connect} from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        await connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await connect(process.env.MONGO_URL);
         console.log('DB Connected');
     } catch (error) {
         console.log(error);
--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-import { ApolloServer, gql } from "apollo-server"
+import { ApolloServer, gql } from "apollo-server";
 
-import typeDefs from "./Schemas/index.js"
-import resolvers from "./Resolvers/index.js"
-import {connectDB} from "./db.js"
-import dotenv from 'dotenv';
-dotenv.config( { path: './.env' } );
+import typeDefs from "./Schemas/index.js";
+import resolvers from "./Resolvers/index.js";
+import { connectDB } from "./db.js";
 // Crea una instancia de Apollo Server
-const server = new ApolloServer({ typeDefs, resolvers, context: ({ req }) => {
-  const token = req.headers['x-token'] || '';
-  return { req, token };
-}});
-
-connectDB()
-
-// Inicia el servidor
-server.listen({
-  port: process.env.PORT || 4000
-}).then(({ url }) => {
-  console.log(`Servidor GraphQL listo en ${url}`);
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+        const token = req.headers["x-token"] || "";
+        return { req, token };
+    },
 });
 
+connectDB();
+
+// Inicia el servidor
+server
+    .listen({
+        port: process.env.PORT || 4000,
+    })
+    .then(({ url }) => {
+        console.log(`Servidor GraphQL listo en ${url}`);
+    });
